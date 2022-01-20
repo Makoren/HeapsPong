343f3fa614f52a81aca9b7e748560cc0193bdae3
Luke Lazzaro <zodiac44@hotmail.com>
2022-01-20 20:20:17 +1100

    Add ball and paddles

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,21 @@
-import hxd.App;
+import hxd.Window;
 
-class Main extends App {
+class Main extends hxd.App {
+    var ball:Ball;
+    var player: Paddle;
+    var opponent: Paddle;
+
+    override function init() {
+        ball = new Ball(s2d);
+        ball.setPosition(Window.getInstance().width / 2, Window.getInstance().height / 2);
+
+        player = new Paddle(s2d);
+        player.setPosition(100, Window.getInstance().height / 2);
+
+        opponent = new Paddle(s2d);
+        opponent.setPosition(Window.getInstance().width - 100, Window.getInstance().height / 2);
+    }
+
     static function main() {
         new Main();
     }
---
+++ src/Paddle.hx
@@ -0,0 +1,11 @@
+import h2d.Tile;
+import h2d.Object;
+import h2d.Bitmap;
+
+class Paddle extends Bitmap {
+    public function new(?parent: Object) {
+        super(Tile.fromColor(0xFFFFFF, 16, 128), parent);
+        tile.dx = -tile.width / 2;
+        tile.dy = -tile.height / 2;
+    }
+}
